6cbe723137de26bc94abccfebef65517fd2032cc
Ninad Sutrave <ninadsutrave@gmail.com>
2023-06-13 02:03:12 +0530

    some notes

---
+++ implementations/priority.cpp
@@ -0,0 +1,24 @@
+/**
+ * Queues, stacks, trees, graphs are all abstract data types. 
+ * They represent an idea of storing and handling data.
+ * For eg. queues represent the idea of storing data in a linear manner,
+ * and handling enqueue and dequeue operations in FIFO fashion.
+ * 
+ * But the underlying data structure used for the implementation of a 
+ * queue could be a linked list or a static array.
+ * 
+ * Similarly, a priority queue is an abstract data type that represents
+ * the idea of storing of comparable data on basis of a defined priority.
+ * The element with the greatest priority maybe removed from the colleciton.
+ * 
+ * And binary heap is a specific data structure which is most commonly used
+ * for the implementation of a priority queue.
+ * 
+ * Other efficient ways of implementing a priority queue:
+ *  • Binary search tree (may have bad performance in worst time 
+ *    if the tree is skewed or highly unbalanced)
+ *  • Self balancing binary search tree
+ *  • Skip list
+ *  • Fibonacci heap
+ * 
+*/
